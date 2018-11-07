84a15d09784f24ec28b2f668a743dfd9658cb0db
shogo.kitamura <shogo.kitamura@accenture.com>
2018-11-07 14:26:50 +0900

    Initial Commit

---
+++ firebase_crud_sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebase_crud_sample
+//
+//  Created by shogo.kitamura on 11/7/18.
+//  Copyright © 2018 shogo.kitamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
